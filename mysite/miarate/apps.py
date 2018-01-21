8601d02cd41246aa6e38e9ea4a27c7be1560791e
tangweihhu <tangweihhu@163.com>
2018-01-21 09:15:41 +0800

    django book

---
+++ mysite/miarate/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MiarateConfig(AppConfig):
+    name = 'miarate'
---
+++ mysite/polls/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import Queation
+# Register your models here.
+
+admin.site.register(Queation)
